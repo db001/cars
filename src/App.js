3ffd6a88ef1ef592a20ac712ed86f34a15b6fba8
db001 <dnbennett@hotmail.co.uk>
2018-05-31 13:06:57 +0100

    Starting...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Car from './components/Car';
+import carData from './db.json';
 
 class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount() {
+    this.setState({
+      carData
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello</h1>
       </div>
     );
   }
